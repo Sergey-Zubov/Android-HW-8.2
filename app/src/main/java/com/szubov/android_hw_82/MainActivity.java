90c172b961256631a342438612c34a880f4851d1
Sergey Zubov <serjio.zubov@gmail.com>
2020-07-08 22:51:55 +0300

    for check

--- app/src/main/java/com/szubov/android_hw_82/MainActivity.java
+++ app/src/main/java/com/szubov/android_hw_82/MainActivity.java
@@ -1,20 +1,23 @@
 package com.szubov.android_hw_82;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.service.quicksettings.Tile;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner mSpinnerLoc;
     private Button mBtnOk;
     private TextView mTextView;
+    private Locale choiceLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +29,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        mSpinnerLoc.findViewById(R.id.spinnerLoc);
-        mBtnOk.findViewById(R.id.btnOk);
-        mTextView.findViewById(R.id.textView);
+        mBtnOk = findViewById(R.id.btnOk);
+        mTextView = findViewById(R.id.textView);
+        mSpinnerLoc = findViewById(R.id.spinnerLoc);
 
         initSpinnerLoc();
 
         mBtnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v) {
-                    case "Русский":
-
-                        break;
+                if (!Locale.getDefault().equals(choiceLoc)) {
+                    changeLoc(choiceLoc);
                 }
             }
         });
@@ -55,7 +56,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String[] language = getResources().getStringArray(R.array.spinner_loc_array);
-                choiceLoc(language[position]);
+                if (language[position].equals("Английский") || language[position].equals("English")) {
+                    choiceLoc = new Locale("en");
+                } else {
+                    choiceLoc = new Locale("ru");
+                }
             }
 
             @Override
@@ -65,7 +70,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void choiceLoc(String s) {
+    private void changeLoc(Locale choiceLoc) {
+        Locale.setDefault(choiceLoc);
+        Configuration configuration = new Configuration();
+        configuration.setLocale(choiceLoc);
+        getResources().updateConfiguration(configuration,
+                getBaseContext().getResources().getDisplayMetrics());
 
+        recreate();
     }
 }
